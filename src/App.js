f15a14cb22742f46c11398eebfc0092dca2ddf23
Антон Сапунков <antonsapunkov@MacBook-Air-Anton.local>
2021-07-04 12:50:29 +0400

    04.07

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Provider as StoreProvider } from 'react-redux';
 
 import store from 'store';
 
-const App = () => <StoreProvider store={store} />;
+const App = () => (
+  <StoreProvider store={store}>
+    <h1>Test</h1>
+  </StoreProvider>
+);
 
 export default App;
